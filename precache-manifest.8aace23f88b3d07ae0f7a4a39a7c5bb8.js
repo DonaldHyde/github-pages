244a79e0932d132ae4cacc6ab6f0ebd0f5eb8efd
Donald Hyde <donaldhyde@Donalds-MacBook-Pro.local>
2019-08-13 01:36:30 -0400

    Updates

---
+++ precache-manifest.8aace23f88b3d07ae0f7a4a39a7c5bb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9b245ea88e175f734ce835c322d6de",
+    "url": "/github-pages/index.html"
+  },
+  {
+    "revision": "5e080683b5a6f20398d7",
+    "url": "/github-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8f860022f0dafed3d41e",
+    "url": "/github-pages/static/js/2.4ba2e4ef.chunk.js"
+  },
+  {
+    "revision": "5e080683b5a6f20398d7",
+    "url": "/github-pages/static/js/main.11fb602d.chunk.js"
+  },
+  {
+    "revision": "1ac53710565525da4ac9",
+    "url": "/github-pages/static/js/runtime~main.421f9e4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
